eb3602d9068d7da7fe04955ab29964a2977ce373
CReies <ccreyesmolina@gmail.com>
2023-05-24 19:09:06 -0500

    delete unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import 'dotenv-safe/config';
-import add from '@math/add';
 import createDebug from 'debug';
 
 const log = createDebug('index');
 log.enabled = true;
 
 log(process.env.NAME);
-log(add(1, 2));
