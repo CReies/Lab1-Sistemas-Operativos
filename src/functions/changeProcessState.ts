bfb829d7497b65a675d5b5422e392579657bca46
CReies <ccreyesmolina@gmail.com>
2023-05-25 00:04:58 -0500

    add a function that changes the state of a process

---
+++ src/functions/changeProcessState.ts
@@ -0,0 +1,9 @@
+import { process } from '@src/types';
+
+const changeProcessState = (process: process, newState: process['state']) => {
+	console.log(`${process.name} #${process.PID} - ${process.state} => ${newState}`);
+	process = { ...process, state: newState };
+	return process;
+};
+
+export default changeProcessState;
