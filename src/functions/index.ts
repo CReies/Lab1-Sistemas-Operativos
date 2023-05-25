d8d30e4ffffdb4a2752037c005170d1a0faebb98
CReies <ccreyesmolina@gmail.com>
2023-05-25 02:13:08 -0500

    add sleep function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,3 +1,4 @@
 export { default as readJSON } from './readJSON';
 export { default as writeJSON } from './writeJSON';
 export { default as changeProcessState } from './changeProcessState';
+export { default as sleep } from './sleep';
