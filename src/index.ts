76d8106a24d06521db19b133b700b7773371a7f3
CReies <ccreyesmolina@gmail.com>
2023-05-25 02:13:35 -0500

    add CLI

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
+import * as readline from 'readline';
+import { FIFO } from './algorithms';
+import { readJSON } from './functions';
+import { process } from './types';
 
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const processes = readJSON('./src/processes.json') as process[];
 
+function ask() {
+	rl.question('Que algoritmo de planificación le gustaría utilizar?\n1- FIFO\n2- ', input => {
+		switch (input) {
+			case '1':
+				rl.close();
+				FIFO(processes, 1000);
+				rl.close();
+				break;
+
+			default:
+				console.log('\nPor favor ingrese un valor válido');
+				setTimeout(() => {
+					console.log();
+					ask();
+				}, 2000);
+				break;
+		}
+	});
+}
+
+ask();
