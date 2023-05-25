54c12bf293cbe4eef3fe0fd525e26937a80b641f
CReies <ccreyesmolina@gmail.com>
2023-05-25 03:55:08 -0500

    change modification way

--- src/functions/changeProcessState.ts
+++ src/functions/changeProcessState.ts
@@ -2,7 +2,7 @@ import { process } from '../types';
 
 const changeProcessState = (process: process, newState: process['state']) => {
 	console.log(`${process.name} #${process.PID} - ${process.state} => ${newState}`);
-	process = { ...process, state: newState };
+	process.state = newState;
 	return process;
 };
 
