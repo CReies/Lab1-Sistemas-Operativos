a909fb5942d9b6845d9d8a6d2aeaaa6c1182f332
CReies <ccreyesmolina@gmail.com>
2023-05-25 00:03:47 -0500

    change data type from Object to object

--- src/functions/writeJSON.ts
+++ src/functions/writeJSON.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-const writeJSON = (route: string, data: Object): void => {
+const writeJSON = (route: string, data: object): void => {
 	const content = JSON.stringify(data, null, 2);
 	fs.writeFileSync(route, content, 'utf-8');
 };
